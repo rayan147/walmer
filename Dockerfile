4a8f7185d956a42f9b60215dfcd77ab83f4f201b
Rayan Ramirez <rayan361@gmail.com>
2022-04-02 14:06:51 -0400

    initial commit to the current branch

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# pull nodejs runtime
+FROM node:17.8.0
+# create a dir to install the app
+WORKDIR /app
+
+COPY package*.json /app
+
+# install dependencies
+RUN npm install
+
+# copy the app to the /app directory
+COPY . /app
+
+# expose the app port
+EXPOSE 5000
+
+CMD [ "node", "server.js" ]
+
+
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express()
+const PORT = 5000
+
+// set route "/"
+app.get('/',(req,res) => {
+  res.json({
+      greeting: "Hello World ! "
+  })
+})
+
+// set route /costco
+// return   always!
+
+app.get('/costco',(req,res)=>{
+   res.json({
+      warehouse:" always"
+   })
+})
+app.get('/costco2',(req,res)=>{
+  res.json({
+     warehouse:" always 2"
+  })
+})
+// set server to listen on PORT 5000
+app.listen(PORT,()=> console.log(`Webserver is listening on port ${PORT}`))
+
