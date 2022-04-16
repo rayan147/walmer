6448c64411da8d943810354c93e213bd6e58bffd
Rayan Ramirez <rayan361@gmail.com>
2022-04-16 17:34:03 -0400

    ci(putting_together_the_project) add npm install

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.get('/costco2',(req,res)=>{
      warehouse:" always 2"
   })
 })
+app.get('/billing',(req,res)=>{
+  res.json({
+     owed:"You owe $100"
+  })
+})
 // set server to listen on PORT 5000
 app.listen(PORT,()=> console.log(`Webserver is listening on port ${PORT}`))
 
